e890ba6ccdaf8d4c5a0c1f8c40457ad4a25dd80a
manojravichandran <manojravichandran18@gmail.com>
2019-07-11 19:10:13 +0530

    nothing just routing an+exp

--- vstorage/server.js
+++ vstorage/server.js
@@ -5,7 +5,7 @@ const path=require('path');
 const api=require('./server/routes/api');
 
 const port =3000;
-
+const router=express.Router();
 const app=express();
 
 app.use(express.static(path.join(__dirname,'dist/vstorage')));
@@ -16,7 +16,7 @@ app.use(bodyparser.json());
 
 app.use('/api',api);
 
-app.get('*',(req,res) => {
+app.get(' ',(req,res) => {
 
     res.sendFile(path.join(__dirname,'dist/index.html'));
 });
