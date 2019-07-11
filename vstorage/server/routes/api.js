f101acdd2c95e616bf30d43f0f5cbb7148525642
manojravichandran <manojravichandran18@gmail.com>
2019-07-11 22:06:38 +0530

    prob concting atlas with app.5-20 prob

--- vstorage/server/routes/api.js
+++ vstorage/server/routes/api.js
@@ -2,7 +2,22 @@ const express=require('express');
 
 const router=express.Router();
 const mongoose=require('mongoose');
+const uri = "mongodb+srv://<>:<>@cluster0-dw8dl.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
+
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect(uri,{ useNewUrlParser: true },function(err){
+    
+
+if(err)
+{
+    console.log("Error !"+err);
+}
+
+
+});
 router.get('/',function(req,res){
 
     res.send('<h1>hey there</h1>');
