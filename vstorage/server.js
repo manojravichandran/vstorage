e12a8c8a27cea87834793820410080005f4c68f9
manojravichandran <manojravichandran18@gmail.com>
2019-07-11 18:48:55 +0530

    added route : server.js,crted fle dis/rout/api.js

---
+++ vstorage/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const path=require('path');
+
+const api=require('./server/routes/api');
+
+const port =3000;
+
+const app=express();
+
+app.use(express.static(path.join(__dirname,'dist/vstorage')));
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(bodyparser.json());
+
+app.use('/api',api);
+
+app.get('*',(req,res) => {
+
+    res.sendFile(path.join(__dirname,'dist/index.html'));
+});
+
+
+app.listen(port,function(){
+
+    console.log("running in 3000"+port);
+});
---
+++ vstorage/server/routes/api.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',function(req,res){
+
+    res.send('<h1>hey there</h1>');
+});
+
+module.exports=router;
